e6081712470bb4b1daa3b57d14fe52df92769263
Nilay <n.prajapati2016@gmail.com>
2022-09-10 18:02:05 -0500

    Fixed email sending animation

--- index.js
+++ index.js
@@ -6,18 +6,18 @@ function contact(event){
     event.preventDefault();
     const loading = document.querySelector('.modal__overlay--loading')
     const success = document.querySelector('.modal__overlay--success')
-    loading.classList += " .modal__overlay--visible"
+    loading.classList += " modal__overlay--visible"
     emailjs
     .sendForm(
         'service_nkimoa4',
         'template_016s4er',
         event.target,
-        'aTw2n1gEB4NAVTnbM'
+        'K7mlq1iad9TFVmDgC'
     ).then(() => {
-        loading.classList.remove(".modal__overlay--visible")
-        success.classList += " .modal__overlay--visible"
+        loading.classList.remove("modal__overlay--visible")
+        success.classList += " modal__overlay--visible"
     }).catch(() => {
-        loading.classList.remove(".modal__overlay--visible")
+        loading.classList.remove("modal__overlay--visible")
         alert(
             "The email service is temporarily unavailable, please contact me directly at n.prajapati2016@gmail.com"
         )
