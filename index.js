f4158367364317b51763c651b73c6e0a61645d7d
Nilay <n.prajapati2016@gmail.com>
2022-09-11 16:50:59 -0500

    Added dark theme

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-//template_016s4er
-//service_nkimoa4
-//aTw2n1gEB4NAVTnbM
+let isModalOpen = false
+let contrastToggle = false
+
+function toggleContrast(){
+    contrastToggle = !contrastToggle
+    if(contrastToggle){
+    document.body.classList += " dark-theme"
+    }
+    else{
+        document.body.classList.remove("dark-theme")
+    }
+}
 
 function contact(event){
     event.preventDefault();
@@ -24,7 +33,6 @@ function contact(event){
     })
 }
 
-let isModalOpen = false
 function toggleModal(){
     if(isModalOpen){
         isModalOpen = false
